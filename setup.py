bc158d0603f159892dbfe6586b8c59b260989301
FinByz <info@finbyz.tech>
2024-05-29 12:59:03 +0530

    update in setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
+from pip._internal.req import parse_requirements
+from pip._internal.network.session import PipSession
 
-# get version from __version__ variable in tyco/__init__.py
+# get version from __version__ variable in ibt_call_center/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('tyco/__init__.py', 'rb') as f:
+with open('ibt_call_center/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+# Parsing requirements.txt using pip's internal API
+requirements = parse_requirements("requirements.txt", session=PipSession())
 
 setup(
-	name='tyco',
-	version=version,
-	description='Custom App for Tyco',
-	author='Finbyz Tech Pvt Ltd',
-	author_email='info@finbyz.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+    name='ibt_call_center',
+    version=version,
+    description='Call Center',
+    author='IBT',
+    author_email='callcenteribt@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[str(ir.requirement) for ir in requirements],
+    dependency_links=[str(ir.link.url) for ir in requirements if ir.link]
 )
